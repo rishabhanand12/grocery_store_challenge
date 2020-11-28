30c055fd23aec3f8ab992d70624ad1cb9868f630
Rishabh <mailrishabh.ra@gmail.com>
2020-11-28 12:06:34 +0530

    renamed variable price_list

--- item.rb
+++ item.rb
@@ -5,14 +5,14 @@ class Cart
   end
   
   def calc_price
-    if $arr[@item]['sale']
-      if @qty >= $arr[@item]['sale_eligible_qty']
-        ((@qty / $arr[@item]['sale_eligible_qty']) * ($arr[@item]['sale_price'] * $arr[@item]['sale_eligible_qty'])) + ($arr[@item]['unit_price'] * (@qty % $arr[@item]['sale_eligible_qty']))
+    if $price_list[@item]['sale']
+      if @qty >= $price_list[@item]['sale_eligible_qty']
+        ((@qty / $price_list[@item]['sale_eligible_qty']) * ($price_list[@item]['sale_price'] * $price_list[@item]['sale_eligible_qty'])) + ($price_list[@item]['unit_price'] * (@qty % $price_list[@item]['sale_eligible_qty']))
       else
-        @qty * $arr[@item]['unit_price']
+        @qty * $price_list[@item]['unit_price']
       end
     else
-      @qty * $arr[@item]['unit_price']
+      @qty * $price_list[@item]['unit_price']
     end
   end
 end
