9c1c50930bda8ad003b624c88e1fa53138c03e99
Rishabh <mailrishabh.ra@gmail.com>
2020-11-28 11:51:52 +0530

    add items with final prices in cart

--- main.rb
+++ main.rb
@@ -1,8 +1,25 @@
 @require_relative "item"
 
+def createCheckOutBill(cart)
+  items_in_cart = []
+  cart.each do |item, qty|
+    newItem = Item.new(item, qty)
+    # items_in_cart.push({
+    #             'item' => item,
+    #             'qty' => qty,
+    #             'price' => newItem.calc_price
+    #           })
+    items_in_cart.push([item, qty, newItem.calc_price])
+  end
+  items_in_cart
+end
+
 def main
   puts 'enter the list of items seperated by a comma'
   list = gets.chomp.downcase.gsub(/\s+/, '').split(',')
+  cart = {}
+  list.each { |item| createItemList item, cart }
+  checkOutBill = createCheckOutBill cart
 end
 
-main
+main 
