39cdc40fa75a9adfb485e0bf97baf93c176d7c23
Rishabh <mailrishabh.ra@gmail.com>
2020-11-28 11:39:49 +0530

    price table added

--- main.rb
+++ main.rb
@@ -1,36 +1,10 @@
-def createItemList(item, cart)
-  case item
-    when 'milk'
-      if bill['milk'].nil?
-        bill['milk'] = 1
-      elsif bill['milk'] += 1
-      end
-  
-    when 'bread'
-      if bill['bread'].nil?
-        bill['bread'] = 1
-      elsif bill['bread'] += 1
-      end
-  
-    when 'apple'
-      if bill['apple'].nil?
-        bill['apple'] = 1
-      elsif bill['apple'] += 1
-      end
-  
-    when 'banana'
-      if bill['banana'].nil?
-        bill['banana'] = 1
-      elsif bill['banana'] += 1
-      end
-    end
-  end
+@require_relative "price"
+
+$price_list = PriceTable.new.price_list
 
 def main
   puts 'enter the list of items seperated by a comma'
   list = gets.chomp.downcase.gsub(/\s+/, '').split(',')
-  cart = {}
-  list.each { |item| createItemList item, cart }
 end
 
 main
